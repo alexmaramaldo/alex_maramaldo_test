4094b398c3d4f1d2e4895830977beab550c6af4e
Alex Maramaldo <alex@osbox.com.br>
2020-02-02 10:35:19 -0300

    creating the first example to run the test A

---
+++ test_a.py
@@ -0,0 +1,10 @@
+from my_libraries.overlap import Overlap
+
+
+true_result = Overlap((1, 5), (2, 6))
+
+false_result = Overlap((1, 5), (6, 8))
+
+
+print(f"""True example result is: {true_result}""")
+print(f"""False example result is: {false_result}""")
