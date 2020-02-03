c59bb14adc8eff5f81ad8d261976600aebf2064b
Alex Maramaldo <alex@osbox.com.br>
2020-02-02 22:26:10 -0300

    changing the example

--- test_c.py
+++ test_c.py
@@ -3,9 +3,10 @@ from my_libraries.cache_handler import cache_method
 # Using only the annotations, is possible to create simples caches data
 @cache_method
 def teste_cache(param1, param2):
-    return 1
+    return param1 + " " + param2
 
 
 result = teste_cache("Alex", "Maramaldo")
 print(result)
-result = teste_cache("Alex", "Maramaldo")
+result = teste_cache("Alex", "Maramaldo2")
+print(result)
