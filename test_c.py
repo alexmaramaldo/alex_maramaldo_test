ef3bad4687228e90e5df448b59160b85079bdd95
Alex Maramaldo <alex@osbox.com.br>
2020-02-02 21:59:12 -0300

    adding comments

--- test_c.py
+++ test_c.py
@@ -1,6 +1,6 @@
 from my_libraries.cache_handler import cache_method
 
-
+# Using only the annotations, is possible to create simples caches data
 @cache_method
 def teste_cache(param1, param2):
     return 1
